4db6c89946a0f4b28d274508e4875907e3c11ddd
Samuel Tom Joseph <msm19b002@iiitdm.ac.in>
2021-06-13 23:13:15 +0530

    secureder

--- main/settings.py
+++ main/settings.py
@@ -138,7 +138,7 @@ LOGIN_URL = '/login/'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'samtomann2@gmail.com'
-EMAIL_HOST_PASSWORD = 'bbhvsiriynvxbgsn'
+EMAIL_HOST_PASSWORD = '****************'
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
