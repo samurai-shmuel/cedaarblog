678b4086320c012eb33fad4d73bcba9442b372b8
samurai-shmuel <79624130+samurai-shmuel@users.noreply.github.com>
2023-05-14 22:34:12 +0530

    Update settings.py
    shift to railway 1

--- main_app/settings.py
+++ main_app/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = ['cedaarblog.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['cedaarblog.herokuapp.com', 'cedaarblog.up.railway.app', '127.0.0.1']
 
 # Application definition
 
