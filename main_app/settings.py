4a29b0f7c21424bc66bd90c653c47972993bbad7
Samuel Tom Joseph <samtomann@gmail.com>
2023-05-28 23:24:59 +0530

    render

--- main_app/settings.py
+++ main_app/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = ['cedaarblog.herokuapp.com', 'cedaarblog.up.railway.app', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -99,7 +99,7 @@ DATABASES['default']['CONN_MAX_AGE'] = 500
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
